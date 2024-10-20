1a5d72ec98f00d83a33cc49f51ae69e6a7f07d3c
Rodrigo <rodrigobaalves.2004@gmail.com>
2024-10-19 23:37:03 -0300

    formatando como deve ser passado o cnpj e telefone

--- PIM/Models/FornecedorModel.cs
+++ PIM/Models/FornecedorModel.cs
@@ -9,10 +9,12 @@ namespace PIM.Models
         public string Nome_fornecedor { get; set; }
         [Required(ErrorMessage = "Digite o nome do produto")]
         public string Nome_produto { get; set; }
-        [Required(ErrorMessage = "Digite CNPJ")]
+        [Required(ErrorMessage = "O CNPJ é obrigatório.")]
+        [RegularExpression(@"^\d{2}\.\d{3}\.\d{3}/\d{4}-\d{2}$",
+        ErrorMessage = "O formato do CNPJ deve ser XX.XXX.XXX/0001-XX.")]
         public string Cnpj { get; set; }
         [Required(ErrorMessage = "Digite o telefone")]
-        [Phone(ErrorMessage = "O numero informado não é válido")]
+        [RegularExpression(@"^\(\d{2}\)\s9\d{4}-\d{4}$", ErrorMessage = "O formato do telefone celular deve ser (XX) 9XXXX-XXXX.")]
         public string Telefone { get; set; }
 
     }
